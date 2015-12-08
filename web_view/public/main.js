55667d27419dc95acfabd88bf899db083d86751a
Brett VanDam <brettvd@umich.edu>
2015-12-08 00:19:09 -0500

    Mostly There
    Most of the functionality is done. Just need to make it update in
    real-time now. Maybe add some custom filters to the data?

--- web_view/public/main.js
+++ web_view/public/main.js
@@ -19,6 +19,7 @@ function Node(node) {
     self.currentTime = ko.observable(date.toLocaleTimeString() + " " + date.toLocaleDateString());
     self.parentId = ko.observable(node.ParentId);
     
+    self.id = node.Id;
     self.isSink = ko.observable(node.IsSink);
     self.currentState = ko.observable(node.CurrentState);
     self.pathFailedId = ko.observable(node.PathFailedId);
@@ -39,12 +40,33 @@ function Node(node) {
     self.lastBootedTime = ko.observable(node.LastBootedTime);
     
     self.isParentHighlight = ko.observable(false);
-    self.status = ko.observable("good");
+    self.nodeBeingViewed = ko.observable(false);
+    self.status = ko.computed(function() {
+        switch(self.currentState()) {
+            case 0:
+            case 4:
+                return "good";
+            case 8:
+            case 64:
+            case 1024:
+                return "warning";
+            case 1:
+            case 2:
+            case 16:
+            case 32:
+            case 128:
+            case 256:
+                return "error";
+            default:
+                return "warning";
+            }
+    });
 }
 
 function NodeListViewModel() {
     var self = this;
     self.nodes = ko.observableArray([]);
+    self.isLoading = ko.observable(true);
     
     self.distinctNodes = ko.computed(function(){
         var distinct = _.uniq(self.nodes(), 'nodeId' );
@@ -113,16 +135,112 @@ function NodeListViewModel() {
         pNode.isParentHighlight(!pNode.isParentHighlight());
     }
     
+    self.rawDataModalVisible = ko.observable(false);
+    self.historyModalVisible = ko.observable(false);
+    
+    self.rawDataModalVisible.subscribe(function(newVal) {
+        self.togglePopover(newVal);
+    });
+    
+    self.historyModalVisible.subscribe(function(newVal) {
+        self.togglePopover(newVal);
+    });
+    
+    self.togglePopover = function(newVal) {
+        var popover = $(".popover");
+        if(newVal)
+            popover.hide();
+        else
+            popover.show();
+        
+    }
+    
+    self.modalHeaderLabel = ko.observable("");
+    self.selectedNode = ko.observable({});
+    self.rawDataMessage = ko.observable({});
+    self.fullHistoryData = ko.observable({});
+    self.isLoadingFullHistory = ko.observable(false);
+    
+    self.showRawData = function(nodeId) {
+        var node = _.findWhere(self.distinctNodesHierarchy(), {nodeId: nodeId});
+        self.rawDataMessage(node);
+        self.modalHeaderLabel("Raw Message #" + node.id);
+        self.rawDataModalVisible(true);
+    }
+    
+    self.showFullHistory = function(nodeId) {
+        self.isLoadingFullHistory(true);
+        self.historyModalVisible(true);
+        self.modalHeaderLabel("Message History for Node " + nodeId);
+        $.ajax({
+                url: 'http://127.0.0.1:9250/getHistory',
+                type: 'GET',
+                dataType: 'json',
+                data: {nodeId: nodeId},
+                success: function(data) {
+                    self.fullHistoryData($.map(data, function(node) { return new Node(node) }));
+                    self.isLoadingFullHistory(false);
+                    console.log(self.fullHistoryData());
+                },
+                error: function(err) { alert(err.responseText); }
+        });
+    }
+    
+    self.viewNode = ko.observable(self.nodes()[0]);
+    self.changeSelectedMessage = function(node) {
+        if(self.viewNode())
+            self.viewNode().nodeBeingViewed(false);
+        self.viewNode(node);
+        self.viewNode().nodeBeingViewed(true);
+    }
+    
+    self.test = function(data, key) {
+        console.log(data);
+        console.log(key);
+        return true;
+    }
+    
     $.ajax({
             url: 'http://127.0.0.1:9250/update',
             type: 'GET',
             dataType: 'json',
             success: function(data) {
                 self.nodes($.map(data, function(node) { return new Node(node) }));
-                //console.log(self.nodes());
+                self.isLoading(false);
             },
             error: function(err) { alert(err.responseText); }
     });
 }
+
+//-----------------------Spinner----------------------------------
+ko.bindingHandlers.spinner = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        var options = {};
+        $.extend(options, ko.bindingHandlers.spinner.defaultOptions, ko.utils.unwrapObservable(allBindingsAccessor().spinnerOptions));
+        
+        //create the spinner with the appropriate options
+        var spinner = new Spinner(options);
+        
+        //could do this in the update function, but this gives us easy access to the spinner object (through the closure) without having to store it with the element
+        ko.dependentObservable(function() {
+            var value = ko.utils.unwrapObservable(valueAccessor());  //this ensures that it depends on the observable value
+            if (value) {
+               spinner.spin(element);    
+            } else if (spinner.el) {   //don't stop first time
+                spinner.stop(); 
+            }
+        });     
+    },
+    defaultOptions: {
+        lines: 10,
+        length: 3,
+        width: 3,
+        radius: 5,
+        color: '#000',
+        speed: 1,
+        trail: 75,
+        shadow: true
+    }
+};
 
 ko.applyBindings(new NodeListViewModel());
--- web_view/server.js
+++ web_view/server.js
@@ -18,9 +18,22 @@ app.get('/', function(req, res) {
 
 app.get('/update', function(req, res) {
     var db = new sqlite3.Database('./824Sim.db');
-    db.all("SELECT * FROM NodeModel ORDER BY NodeId ASC, CurrentTime DESC;", function(err, rows) {
+    var query = "SELECT * FROM NodeModel WHERE ( SELECT COUNT(*) FROM NodeModel as n WHERE n.NodeId = NodeModel.NodeId AND n.CurrentTime >= NodeModel.CurrentTime) <= 10 ORDER BY NodeId, CurrentTime DESC;";
+    /*"SELECT * FROM NodeModel ORDER BY NodeId ASC, CurrentTime DESC;"*/
+    db.all(query, function(err, rows) {
         if(err) throw err;
         res.json(rows);
     });
+    db.close();
+});
+
+app.get('/getHistory', function(req, res) {
+    var db = new sqlite3.Database('./824Sim.db');
+    var query = "SELECT * FROM NodeModel WHERE NodeId = " + req.query.nodeId + " ORDER BY CurrentTime DESC;";
+    db.all(query, function(err, rows) {
+        if(err) throw err;
+        res.json(rows);
+    });
+    
     db.close();
 });
